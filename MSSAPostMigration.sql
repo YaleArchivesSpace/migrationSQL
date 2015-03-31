bfa3954e5723570cafdf896616be06691c72ba7b
Maureen Callahan <maureen.callahan@gmail.com>
2015-03-31 16:52:36 -0400

    Update MSSAPostMigration.sql

--- MSSAPostMigration.sql
+++ MSSAPostMigration.sql
@@ -1,22 +1,9 @@
 /* This looks for any content in user_defined.boolean_1 that is associated with an MSSA accession record, deletes it, and associates it with material_type. *?
 /* WORK IN PROGRESS */
 use testdb_aspace;
-SELECT 
-    user_defined.accession_id,
-    user_defined.boolean_1,
-    accession.id,
-    accession.repo_id,
-    repository.id,
-    repository.repo_code
-FROM
-    accession 
-        JOIN
-    repository ON accession.repo_id = repository.id
-        JOIN
-    user_defined ON user_defined.accession_id = accession.id
-WHERE
-    repository.repo_code = 'mssa'
-        AND user_defined.boolean_1 IS NOT NULL
-
 Insert into material_types (accession_id, electronic_documents)
-select accession_id, '1' from user_defined where user_defined.boolean_1 is not null
+select accession_id, '1' from user_defined 
+join accession on user_defined.accession_id = accession.id
+join repository ON accession.repo_id = repository.id
+where user_defined.boolean_1 is not null 
+and repository.repo_code = 'mssa';
