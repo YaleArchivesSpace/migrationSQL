23908d1b4f856ae6e789e2984771bcc51cf0e987
Maureen Callahan <maureen.callahan@gmail.com>
2015-05-13 11:35:56 -0400

    Update FauxCodes.sql

--- FauxCodes.sql
+++ FauxCodes.sql
@@ -5,23 +5,26 @@ Don't forget to delete fauxcodes from ASpace when this is all done! */
 
 /* This script relies on functions found here: https://github.com/yalemssa/ATKreporting */
 
+/* First, count how many fauxcodes you're going to end up with. This should be a count of the distinct, unbarcoded containers in your repository */
 use mssa;
-SELECT 
-    CONCAT('DeleteMe',
+SELECT count(distinct(
+    CONCAT('Faux.',
             CONCAT(r.resourceIdentifier1,
                     LPAD(r.resourceIdentifier2, 4, '00')),
-            '.series',
-            IFNULL(series.subdivisionIdentifier, ''),
-            '.box',
+            IF(series.subdivisionIdentifier <> '',
+                CONCAT('.series', series.subdivisionIdentifier),
+                ''),
+                '.',
+            adi.container1Type,
             IFNULL(adi.container1NumericIndicator, ''),
-            IFNULL(adi.container1AlphaNumIndicator, ''))
+            IFNULL(adi.container1AlphaNumIndicator, '')))) fauxcode
 FROM
     ArchDescriptionInstances adi
         JOIN
     ResourcesComponents rc ON adi.resourceComponentId = rc.resourceComponentId
         INNER JOIN
-    Resources r ON r.resourceId = getResourceFromComponent(rc.resourceComponentId)
+    Resources r ON r.resourceId = GETRESOURCEFROMCOMPONENT(rc.resourceComponentId)
         LEFT OUTER JOIN
-    ResourcesComponents series ON getTopComponent(rc.resourceComponentId) = series.resourceComponentID
+    ResourcesComponents series ON GETTOPCOMPONENT(rc.resourceComponentId) = series.resourceComponentID
 WHERE
     adi.barcode = ''
