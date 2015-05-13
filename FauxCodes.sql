db0dd48e27fbd7f25ac453075bf48204d0d9d7a4
Maureen Callahan <maureen.callahan@gmail.com>
2015-05-13 16:40:20 -0400

    Update FauxCodes.sql

--- FauxCodes.sql
+++ FauxCodes.sql
@@ -82,4 +82,67 @@ SET
             IFNULL(adi.container1NumericIndicator, ''),
             IFNULL(adi.container1AlphaNumIndicator, ''))
 WHERE
-    adi.barcode = ''
+    adi.barcode = '';
+    
+/* deal with common slide boxes*/
+use mssa;
+update archDescriptionInstances set barcode='CS15'
+where barcode like '%CS15%';
+update archDescriptionInstances set barcode='CS14'
+where barcode like '%CS14%';
+update archDescriptionInstances set barcode='CS13'
+where barcode like '%CS13%';
+update archDescriptionInstances set barcode='CS12'
+where barcode like '%CS12%';
+update archDescriptionInstances set barcode='CS11'
+where barcode like '%CS11%';
+update archDescriptionInstances set barcode='CS10'
+where barcode like '%CS10%';
+update archDescriptionInstances set barcode='CS9'
+where barcode like '%CS9%';
+update archDescriptionInstances set barcode='CS8'
+where barcode like '%CS8%';
+update archDescriptionInstances set barcode='CS7'
+where barcode like '%CS7%';
+update archDescriptionInstances set barcode='CS6'
+where barcode like '%CS6%';
+update archDescriptionInstances set barcode='CS5'
+where barcode like '%CS5%';
+update archDescriptionInstances set barcode='CS4'
+where barcode like '%CS4%';
+update archDescriptionInstances set barcode='CS3'
+where barcode like '%CS3%';
+update archDescriptionInstances set barcode='CS2'
+where barcode like '%CS2%';
+update archDescriptionInstances set barcode='CS1'
+where barcode like '%CS1%';
+update archDescriptionInstances set container1AlphaNumIndicator='CS15'
+where barcode like '%CS15%';
+update archDescriptionInstances set container1AlphaNumIndicator='CS14'
+where barcode like '%CS14%';
+update archDescriptionInstances set container1AlphaNumIndicator='CS13'
+where barcode like '%CS13%';
+update archDescriptionInstances set container1AlphaNumIndicator='CS12'
+where barcode like '%CS12%';
+update archDescriptionInstances set container1AlphaNumIndicator='CS11'
+where barcode like '%CS11%';
+update archDescriptionInstances set container1AlphaNumIndicator='CS10'
+where barcode like '%CS10%';
+update archDescriptionInstances set container1AlphaNumIndicator='CS9'
+where barcode like '%CS9%';
+update archDescriptionInstances set container1AlphaNumIndicator='CS8'
+where barcode like '%CS8%';
+update archDescriptionInstances set container1AlphaNumIndicator='CS7'
+where barcode like '%CS7%';
+update archDescriptionInstances set container1AlphaNumIndicator='CS6'
+where barcode like '%CS6%';
+update archDescriptionInstances set container1AlphaNumIndicator='CS5'
+where barcode like '%CS5%';
+update archDescriptionInstances set container1AlphaNumIndicator='CS4'
+where barcode like '%CS4%';
+update archDescriptionInstances set container1AlphaNumIndicator='CS3'
+where barcode like '%CS3%';
+update archDescriptionInstances set container1AlphaNumIndicator='CS2'
+where barcode like '%CS2%';
+update archDescriptionInstances set container1AlphaNumIndicator='CS1'
+where barcode like '%CS1%';
