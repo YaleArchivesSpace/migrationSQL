1a58f5d5d8b97996fab7334da14e8fa281198c51
Maureen Callahan <maureen.callahan@gmail.com>
2015-05-13 11:12:15 -0400

    starting with select

--- FauxCodes.sql
+++ FauxCodes.sql
@@ -4,3 +4,24 @@ This is important for helping the migrator create distinct top containers.
 Don't forget to delete fauxcodes from ASpace when this is all done! */
 
 /* This script relies on functions found here: https://github.com/yalemssa/ATKreporting */
+
+use mssa;
+SELECT 
+    CONCAT('DeleteMe',
+            CONCAT(r.resourceIdentifier1,
+                    LPAD(r.resourceIdentifier2, 4, '00')),
+            '.series',
+            IFNULL(series.subdivisionIdentifier, ''),
+            '.box',
+            IFNULL(adi.container1NumericIndicator, ''),
+            IFNULL(adi.container1AlphaNumIndicator, ''))
+FROM
+    ArchDescriptionInstances adi
+        JOIN
+    ResourcesComponents rc ON adi.resourceComponentId = rc.resourceComponentId
+        INNER JOIN
+    Resources r ON r.resourceId = getResourceFromComponent(rc.resourceComponentId)
+        LEFT OUTER JOIN
+    ResourcesComponents series ON getTopComponent(rc.resourceComponentId) = series.resourceComponentID
+WHERE
+    adi.barcode = ''
